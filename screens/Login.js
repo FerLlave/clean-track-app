3a4ee17f13cc7b69b463f07d1cf8080903c81dee
fer.llave <117306661+FerLlave@users.noreply.github.com>
2024-11-08 19:08:04 -0500

    estilos login

--- screens/Login.js
+++ screens/Login.js
@@ -1,17 +1,74 @@
-import { Text, StyleSheet, View, Image } from 'react-native'
+import { Text, StyleSheet, View, Image, TextInput,TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 
 export default function Login () {
   
     return (
-      <View>
+      <View style= {styles.mainClass}>
         <View>
-            <Text>login de perro</Text>
-            <Image source={ ('../assets/quiamlogo.PNG')} />
+            <Text>Clean Track </Text>
+            <Image source={ require ('../assets/quiamLogo.jpg')} style={styles.profile} />
         </View>
+
+        <View style={styles.card}>
+          <View style={styles.cardText}>
+              <TextInput placeholder='name@example.com' style= {{paddingHorizontal:15}}/>
+          </View>
+          
+          <View style={styles.cardText}>
+            <TextInput placeholder='Password' style= {{paddingHorizontal:15}}/>
+          </View>
+
+          <View>
+            <TouchableOpacity>
+              <Text> Sign In </Text>
+            </TouchableOpacity>
+          </View>
+
+        </View>
+
+
+
       </View>
     )
   };
 
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mainClass:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'white'
+
+  },
+
+  profile:{
+    width:100,
+    height:100,
+    borderRadius:50,
+
+  },
+
+  card:{
+    margin:20,
+    backgroundColor:'white',
+    borderRadius:20,
+    width:'90%',
+    padding:20,
+    shadowColor:'#000',
+    shadowOffset:{
+      width:0,
+      height:2,
+    },
+    shadowOpacity:0.25,
+    shadowRadius:4,
+    elevation:5,
+  },
+  cardText:{
+    paddingVertical:20,
+    backgroundColor:'#cccccc40',
+    borderRadius:30,
+    marginVertical:10
+  }
+})

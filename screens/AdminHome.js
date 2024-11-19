e45eb69acdf3a949582ecc364a8cd4541f1b80b8
fer.llave <117306661+FerLlave@users.noreply.github.com>
2024-11-18 20:40:49 -0500

    admin home

--- screens/AdminHome.js
+++ screens/AdminHome.js
@@ -1,14 +1,80 @@
-import { Text, StyleSheet, View } from 'react-native'
-import React from 'react'
+import { Text, StyleSheet, View,Image,FlatList,
+  StatusBar, } from 'react-native'
+
+import React, {useState} from 'react';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default function AdminHome () {
+
+  const DATA = [
+    {
+      id: '1',
+      title: '101',
+    },
+    {
+      id: '2',
+      title: '102',
+    },
+    {
+      id: '3',
+      title: '103',
+    },
+    {
+      id: '4',
+      title: '104',
+    },
+    {
+      id: '5',
+      title: '105',
+    },
+    {
+      id: '6',
+      title: '106',
+    },
+  ];
+  
+  const Item = ({title}) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
   
     return (
-      <View>
-        <Text>adminHome</Text>
-      </View>
+
+      <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+
+      <Image source={ require ('../assets/quiamhotel.jpg')} style={styles.adminImage} />
+        <FlatList
+          data={DATA}
+          renderItem={({item}) => <Item title={item.title} />}
+          keyExtractor={item => item.id}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
+
+     
+
+      
     )
   };
 
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+  container:{
+    flex:1,
+    justifyContent:'top',
+    alignItems:'center',
+    backgroundColor:'white',
+    paddingTop:5,
+
+  },
+
+  adminImage:{
+    width:400,
+    height:160,
+    
+
+  }
+})

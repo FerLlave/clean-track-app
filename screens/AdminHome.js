522122712b363d56a4917ca861fb16113881b226
fer.llave <117306661+FerLlave@users.noreply.github.com>
2024-11-19 21:31:40 -0500

    status clean/dirty

--- screens/AdminHome.js
+++ screens/AdminHome.js
@@ -10,14 +10,17 @@ export default function AdminHome () {
     {
       id: '1',
       title: '101',
+      clean: false,
     },
     {
       id: '2',
       title: '102',
+      clean: true,
     },
     {
       id: '3',
       title: '103',
+      clean: false,
     },
     {
       id: '4',
@@ -26,86 +29,108 @@ export default function AdminHome () {
     {
       id: '5',
       title: '105',
+      clean: true,
     },
     {
       id: '6',
       title: '106',
+      clean: true,
     },
     {
       id: '7',
       title: '107',
+      clean: true,
     },
     {
       id: '8',
       title: '108',
+      clean: true,
     },
     {
       id: '9',
       title: '109',
+      clean: true,
     },
     {
       id: '10',
       title: '110',
+      clean: true,
     },
     {
       id: '11',
       title: '111',
+      clean: true,
     },
     {
       id: '12',
       title: '201',
+      clean: true,
     },
     {
       id: '13',
       title: '202',
+      clean: true,
     },
     {
       id: '14',
       title: '203',
+      clean: true,
+      
     },
     {
       id: '15',
       title: '204',
+      clean: true,
     },
     {
       id: '16',
       title: '205',
+      clean: true,
     },
     {
       id: '17',
       title: '206',
+      clean: false,
     },
     {
       id: '18',
       title: '207',
+      clean: true,
     },
     {
       id: '19',
       title: '208',
+      clean: true,
     },
     {
       id: '20',
       title: '209',
+      clean: false,
     },
     {
       id: '21',
       title: '210',
+      clean: true,
     },
     {
       id: '22',
       title: '211',
+      clean: true,
     },
     {
       id: '23',
       title: '212',
+      clean: fase,
     },
     {
       id: '24',
       title: '213',
+      clean: true,
     },
     {
       id: '25',
       title: '214',
+      clean: true,
     },
   ];
   
@@ -140,22 +165,22 @@ const styles = StyleSheet.create({
 
   container:{
     flex:1,
-    justifyContent:'top',
-    alignItems:'center',
     backgroundColor:'white',
     paddingTop:5,
 
   },
 
   adminImage:{
+    alignSelf: 'center',
     width:400,
     height:160,
+    marginBottom:10,
     
 
   },
 
   item: {
-   
+   alignSelf:'flex-start',
     backgroundColor: '#f9c2ff',
     padding: 20,
     marginVertical: 8,

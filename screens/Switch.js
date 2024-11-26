01ed851462089bbc03bea80411c0a54a5a451cef
fer.llave <117306661+FerLlave@users.noreply.github.com>
2024-11-25 21:08:05 -0500

    static view

--- screens/Switch.js
+++ screens/Switch.js
@@ -1,100 +1,95 @@
-
 import { useState, useEffect, useRef } from "react";
-import { Animated, View, Text, TouchableWithoutFeedback, StyleSheet } from "react-native";
+import {
+  Animated,
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  StyleSheet,
+} from "react-native";
 
-export default ({temporal, isButton}) => {
+export default ({ temporal, isButton = true }) => {
+  const AnimatedValues = {
+    toogle: useRef(new Animated.Value(0)).current,
+  };
 
-    const AnimatedValues ={
-        toogle:useRef(new Animated.Value(0)).current
+  const { toogle } = AnimatedValues;
 
-    }
+  const [clean, setClean] = useState(temporal);
 
-    const {toogle} = AnimatedValues;
+  useEffect(() => {
+    handleAnimated();
+  }, [clean]);
 
-    const [clean, setClean] = useState(temporal)
+  const handleAnimated = () => {
+    Animated.timing(toogle, {
+      toValue: clean ? 1 : 0,
+      duration: 275,
+      useNativeDriver: false,
+    }).start();
+  };
 
-    useEffect ( () =>{
-       
-        handleAnimated();
+  const animatedStyles = {
+    transform: [
+      {
+        translateX: toogle.interpolate({
+          inputRange: [0, 1],
+          outputRange: [2.5, 25.4],
+          extrapolate: "clamp",
+        }),
+      },
+    ],
+  };
 
-    }, [clean])
-
-    const handleAnimated = () =>{
-        Animated.timing(toogle,{
-            toValue: clean ? 1 : 0,
-            duration:275,
-            useNativeDriver:false
-        }).start();
-
-    }
-
-    const animatedStyles = {
-        transform: [
-            {
-                translateX: toogle.interpolate({
-                    
-                    inputRange:[0,1],
-                    outputRange: [2.5,25.4],
-                    extrapolate:'clamp',
-
-                }),
-                
-            },
-        ],
-    };
-
-    
-
-    return (
-        <View style = {styles.container}>
-            <TouchableWithoutFeedback onPressIn={() => setClean(!clean)}>
-                <View style ={[ styles.button, {backgroundColor:clean ? '#C08552' : '#49B6b6' }]}>
-
-                    <Animated.View style = {[styles.circle, animatedStyles]}/>
-                    <View style = {[styles.title, {left: clean ? 8 :27}]}/>
-
-                  
-
-                </View>
-            </TouchableWithoutFeedback>
-
-
+  return (
+    <View style={styles.container}>
+      {isButton ? (
+        <TouchableWithoutFeedback onPressIn={() => setClean(!clean)}>
+          <View
+            style={[
+              styles.button,
+              { backgroundColor: clean ? "#C08552" : "#49B6b6" },
+            ]}
+          >
+            <Animated.View style={[styles.circle, animatedStyles]} />
+            <View style={[styles.title, { left: clean ? 8 : 27 }]} />
+          </View>
+        </TouchableWithoutFeedback>
+      ) : (
+        <View
+          style={[
+            styles.button,
+            { backgroundColor: clean ? "#C08552" : "#49B6b6" },
+          ]}
+        >
+          <Animated.View style={[styles.circle, animatedStyles]} />
+          <View style={[styles.title, { left: clean ? 8 : 27 }]} />
         </View>
-      
-    )
-}
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-
-    container:{
-        justifyContent: "center",
-        alignItems:"center",
-        borderRadius:5,
-        
-
-    },
-    button: {
-        width:48,
-        height:24.8,
-        justifyContent:'center',
-        borderRadius:35,
-    },
-    circle:{
-
-        width:20,
-        height:20,
-        borderRadius:35,
-        position:'absolute',
-        backgroundColor:'white',
-        left:0,
-
-
-    },
-    title:{
-        position:'absolute',
-    }
-    
-})
-
-
-
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  button: {
+    width: 48,
+    height: 24.8,
+    justifyContent: "center",
+    borderRadius: 35,
+  },
+  circle: {
+    width: 20,
+    height: 20,
+    borderRadius: 35,
+    position: "absolute",
+    backgroundColor: "white",
+    left: 0,
+  },
+  title: {
+    position: "absolute",
+  },
+});
